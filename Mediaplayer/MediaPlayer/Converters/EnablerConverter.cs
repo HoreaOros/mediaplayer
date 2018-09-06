a961e01980f39e125988e6a2be7d384732036262
Borz Alexandru <alexandru.borz94@gmail.com>
2018-09-06 08:48:11 +0300

    Initial commit

---
+++ Mediaplayer/MediaPlayer/Converters/EnablerConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Data;
+
+namespace MediaPlayer.Converters
+{
+    class EnablerConverter : IMultiValueConverter
+    {
+        /*
+         * takes an array of values convertables to string and return true if all of them have length
+         */
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            //if there are less than 2 values something is wrong
+            if (values.Count() < 2){ return false; }
+            //only need to check two values so from [3]... is irrelevant
+            return (((string)values[0]).Length > 0 && ((string)values[1]).Length > 0);
+        }
+        //not needed
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Mediaplayer/MediaPlayer/Converters/HighlightConverter.cs
@@ -0,0 +1,20 @@
+﻿using MediaPlayer.Controls;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MediaPlayer.Converters
+{
+    class HighlightConverter : IValueConverter
+    {
+        public object Convert(object value, Type TargetType, object parameter, CultureInfo culture)
+        {
+            MediaPlayerThePlayer test = (MediaPlayerThePlayer)value;
+            return test.CurrentIndexPlaying;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
